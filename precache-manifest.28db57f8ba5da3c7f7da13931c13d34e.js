41f38906842440385c896ceb50b5fa387221d3e3
vishalgangwal <gangwalvishal99@gmail.com>
2020-09-06 13:46:35 +0530

    Updates

---
+++ precache-manifest.28db57f8ba5da3c7f7da13931c13d34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc3c352f4253433022bad0cc1c0b7fa",
+    "url": "/monsterapp/index.html"
+  },
+  {
+    "revision": "1cd42c363c3b6766592c",
+    "url": "/monsterapp/static/css/main.3c94a405.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monsterapp/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterapp/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd42c363c3b6766592c",
+    "url": "/monsterapp/static/js/main.67c7e987.chunk.js"
+  },
+  {
+    "revision": "95b43de4aba49e2a18c7",
+    "url": "/monsterapp/static/js/runtime-main.374f5ff6.js"
+  }
+]);
